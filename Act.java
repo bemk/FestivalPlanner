2c118d0be70342a682e73d1295de71a62008ebc9
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-02-09 22:22:36 +0000

    Retrying my commit
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@13 1b5adb80-8662-4049-b0b6-9cde8559a458

--- Act.java
+++ Act.java
@@ -1,22 +1,57 @@
-package fp;
+//package fp;
 
 import java.util.*;
+import java.awt.Color;
 
-public class Act 
+public class Act extends TimeLine
 {
- 
 	private Calendar startTime;
-	 
 	private int endTime;
-	 
 	private ArrayList<Artist> artists;
-	 
 	private String description;
-	 
 	private int duration;
-	
-	
-	 
+	private Color color;
+	private TimeLine supremeTimeLine;
+	private String genre;
 
+	public void setStartTime(Calendar c)
+	{
+		startTime = c;
+	}
+	public Calendar getStartTime()
+	{
+		return startTime;
+	}
+
+	public void setDuration(int d)
+	{
+		duraton = d;
+	}
+	public int getDuration()
+	{
+		return duration;
+	}
+
+	public void addArtist(Artist a)
+	{
+		artists.add(a);
+	}
+	public void removeArtist(int a)
+	{
+		artists.remove(a);
+	}
+	public void removeArtist(Artist a)
+	{
+		this.removeArtist(indexOf(a));
+	}
+
+	public void setDescription(String d)
+	{
+		description = d;
+	}
+	public String getDescription()
+	{
+		return d;
+	}
 }
  
--- Stage.java
+++ Stage.java
@@ -1,5 +1,15 @@
-package fp;
+//package fp;
 
-public class Stage {
+public class Stage extends TimeLine
+{
+	private int id;
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	private int getId()
+	{
+		return id;
+	}
 
 }
