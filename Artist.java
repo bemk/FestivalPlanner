a6a595b52fc169f852ba0005cd24d7cbebd8f66c
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-02-09 18:58:20 +0000

    Added the data storage layer for the address book feature
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@11 1b5adb80-8662-4049-b0b6-9cde8559a458

--- Artist.java
+++ Artist.java
@@ -1,10 +1,44 @@
-package fp;
+//package fp;
 
 public class Artist {
 	private String name;
 	private String preferences;
 	private String comments;
 	private int rating;
-	private String genre;
 
+	public void setName(String n)
+	{
+		name = n;
+	}
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setPreferences(String p)
+	{
+		preferences = p;
+	}
+	public String getPreferences()
+	{
+		return preferences;
+	}
+
+	public void setComments(String c)
+	{
+		comments = c;
+	}
+	public String getComments()
+	{
+		return comments;
+	}
+
+	public void setRating (int r)
+	{
+		rating = r;
+	}
+	public int getRating()
+	{
+		return rating;
+	}
 }
