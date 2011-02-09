cc322006f285e0d6689c56e38b0c80a3e6b6c326
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-02-09 18:58:14 +0000

    Created the address book data layer
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@10 1b5adb80-8662-4049-b0b6-9cde8559a458

---
+++ AddressBook.java
@@ -0,0 +1,29 @@
+package fp;
+
+import java.util.*;
+
+public class AddressBook
+{
+	private ArrayList<Artist> artists = new ArrayList<Artist>();
+
+	public Artist getArtist(int id)
+	{
+		return artists.get(id);
+	}
+	public void addArtist(Artist a)
+	{
+		artists.add(a);
+	}
+	public void removeAtrist(Artist a)
+	{
+		this.removeArtist(artists.indexOf(a));
+	}
+	public void removeArtist(int a)
+	{
+		artists.remove(a);
+	}
+	public ArrayList<Artist> getAllArtists()
+	{
+		return artists;
+	}
+}
