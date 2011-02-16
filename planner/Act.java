5d891acd7cd73db6ee8b90d2bbedb0f7d0d2c473
jpienbro <jpienbro@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-02-16 11:35:12 +0000

    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@64 1b5adb80-8662-4049-b0b6-9cde8559a458

--- planner/Act.java
+++ planner/Act.java
@@ -1,86 +1,93 @@
-//package fp;
+package fp;
 
 import java.util.*;
 import java.awt.Color;
 
 public class Act extends TimeLine
 {
-	private Calendar startTime;
-	private int endTime;
-	private ArrayList<Artist> artists;
-	private String description;
-	private int duration;
-	private Color color;
-	private TimeLine supremeTimeLine;
-	private String genre;
+        private Calendar startTime;
+        private int endTime;
+        private ArrayList<Artist> artists;
+        private String description;
+        private int duration;
+        private Color color;
+        private TimeLine supremeTimeLine;
+        private String genre;
 
-	public void setStartTime(Calendar c)
-	{
-		startTime = c;
-	}
-	public Calendar getStartTime()
-	{
-		return startTime;
-	}
+        public void setStartTime(Calendar c)
+        {
+                startTime = c;
+        }
+        public Calendar getStartTime()
+        {
+                return startTime;
+        }
 
-	public void setDuration(int d)
-	{
-		duration = d;
-	}
-	public int getDuration()
-	{
-		return duration;
-	}
+        public void setDuration(int d)
+        {
+                duration = d;
+        }
+        public int getDuration()
+        {
+                return duration;
+        }
 
-	public void addArtist(Artist a)
-	{
-		artists.add(a);
-	}
-	public void removeArtist(int a)
-	{
-		artists.remove(a);
-	}
-	public void removeArtist(Artist a)
-	{
-		this.removeArtist(artists.indexOf(a));
-	}
+        public void addArtist(Artist a)
+        {
+                artists.add(a);
+        }
+        public void removeArtist(int a)
+        {
+                artists.remove(a);
+        }
+        public void removeArtist(Artist a)
+        {
+                this.removeArtist(artists.indexOf(a));
+        }
 
-	public void setDescription(String d)
-	{
-		description = d;
-	}
-	public String getDescription()
-	{
-		return description;
-	}
+        public void setDescription(String d)
+        {
+                description = d;
+        }
+        public String getDescription()
+        {
+                return description;
+        }
 
-	public void setGenre (String g)
-	{
-		genre = g;
-	}
-	public String getGenre()
-	{
-		return genre;
-	}
-	public Stage findStage()
-	{
-		if (!(supremeTimeLine instanceof Stage))
-		{
-			if (!(supremeTimeLine instanceof Act))
-			{
-				Act a = (Act)supremeTimeLine;
-				return a.findStage();
-			}
-			else
-			{
-				return null;
-			}
-		}
-		else
-		{
-			return (Stage)(supremeTimeLine);
-		}
-	}
-
+        public void setGenre (String g)
+        {
+                genre = g;
+        }
+        public String getGenre()
+        {
+                return genre;
+        }
+        
+        public void setColor (Color c)
+        {
+                color = c;
+        }
+        public Color getColor()
+        {
+                return color;
+        }
+        
+//        public Stage findStage()
+//        {
+//                if (supremeTimeLine != instanceof(Stage))
+//                {
+//                        if (supremeTimeLine!= instanceof(Act))
+//                        {
+//                                return supremeTimeLine.findStage();
+//                        }
+//                        else
+//                        {
+//                                return null;
+//                        }
+//                }
+//                else
+//                {
+//                        return supremeTimeLine;
+//                }
+//        }
 }
- 
