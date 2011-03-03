aa093775cbbc9b79254cc035c5ff69d238452d99
jcmklund <jcmklund@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-03-03 15:35:55 +0000

    did some work on drag and drop
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@159 1b5adb80-8662-4049-b0b6-9cde8559a458

--- planner/ActPaint.java
+++ planner/ActPaint.java
@@ -1,6 +1,11 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.*;
 import java.util.*;
+
+import javax.swing.JPanel;
 
 public class ActPaint implements TimeLinePanel
 {
@@ -8,15 +13,18 @@ public class ActPaint implements TimeLinePanel
 	private int act;
 	private int stage;
 	private Interface iface;
-	private TimeLinePanel t;
+	private TimePanel t;
 	private Shape s;
 	
 	private double height;
 	private double width;
 	private double startX;
 	private double topY;
+	
+	
 	public ActPaint(int act, Interface iface, TimePanel t)
 	{
+
 		this.t = t;
 		this.iface = iface;
 		this.stage = t.getID();
@@ -48,6 +56,7 @@ public class ActPaint implements TimeLinePanel
 	
 	public void paintComponent(Graphics2D g2)
 	{
+		g2.translate(0, height()/2);
 		if (Interface.dbg)
 		{
 //			System.out.println((t.width()/7.9)/iface.getDuration(stage, act)*100+ " duration");
@@ -57,8 +66,12 @@ public class ActPaint implements TimeLinePanel
 		}
 //		s = new RoundRectangle2D.Double((t.width()+650)/((iface.getStartTime(stage, act).getMaximum(GregorianCalendar.HOUR_OF_DAY)*60)+(iface.getStartTime(stage, act).getMaximum(GregorianCalendar.MINUTE))), t.height()/20*1.5,100-((t.width()/+650)/iface.getDuration(stage, act)), t.height()/20 * 19 , 10 ,10);
 		s = new RoundRectangle2D.Double(startX, topY, width, height, 10, 10);
+		t.shapes.add(s);
 		g2.setColor(iface.getColor(stage, act));
 		g2.draw(s);
 		g2.fill(s);
+		g2.translate(0,-height()/2);
 	}
+
+
 }
