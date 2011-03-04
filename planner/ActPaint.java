a5e1d2615546765653170ea29b6ffb89fbca2a4f
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-03-04 12:13:09 +0000

    Advanced toward time based act representation.
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/trunk@169 1b5adb80-8662-4049-b0b6-9cde8559a458

--- planner/ActPaint.java
+++ planner/ActPaint.java
@@ -1,11 +1,6 @@
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.geom.*;
 import java.util.*;
-
-import javax.swing.JPanel;
 
 public class ActPaint implements TimeLinePanel
 {
