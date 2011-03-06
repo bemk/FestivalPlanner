85037b09670bdf09302f9dd75e5709db4e98ba5a
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-03-06 12:35:31 +0000

    Merge branch 'master' into HEAD
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/branches/docs@173 1b5adb80-8662-4049-b0b6-9cde8559a458

--- planner/ActPaint.java
+++ planner/ActPaint.java
@@ -8,25 +8,32 @@ public class ActPaint implements TimeLinePanel
 	private int act;
 	private int stage;
 	private Interface iface;
-	private TimeLinePanel t;
+	private TimePanel t;
 	private Shape s;
 	
 	private double height;
 	private double width;
 	private double startX;
 	private double topY;
+	
+	
 	public ActPaint(int act, Interface iface, TimePanel t)
 	{
+
 		this.t = t;
 		this.iface = iface;
 		this.stage = t.getID();
 		this.act = act;
 		
-		this.height = t.height()/10*9;
+		this.height = t.height()/4;
 		this.topY = -this.height/2;
 		Calendar tmpStart = iface.getStartTime(stage, act);
-		int tmpHr = tmpStart.get(Calendar.HOUR);
+		int tmpHr = tmpStart.get(Calendar.HOUR_OF_DAY);
 		int tmpMin = tmpStart.get(Calendar.MINUTE);
+		if (Interface.dbg)
+		{
+			System.out.printf("Time\t%d:%d\n\n", tmpHr, tmpMin);
+		}
 		tmpMin += (tmpHr*60);
 		startX = (t.width()/1440)*tmpMin;
 		width = t.width()/1400*iface.getDuration(stage, act);
@@ -44,6 +51,7 @@ public class ActPaint implements TimeLinePanel
 	
 	public void paintComponent(Graphics2D g2)
 	{
+
 		if (Interface.dbg)
 		{
 //			System.out.println((t.width()/7.9)/iface.getDuration(stage, act)*100+ " duration");
@@ -53,8 +61,11 @@ public class ActPaint implements TimeLinePanel
 		}
 //		s = new RoundRectangle2D.Double((t.width()+650)/((iface.getStartTime(stage, act).getMaximum(GregorianCalendar.HOUR_OF_DAY)*60)+(iface.getStartTime(stage, act).getMaximum(GregorianCalendar.MINUTE))), t.height()/20*1.5,100-((t.width()/+650)/iface.getDuration(stage, act)), t.height()/20 * 19 , 10 ,10);
 		s = new RoundRectangle2D.Double(startX, topY, width, height, 10, 10);
+		t.shapes.add(s);
 		g2.setColor(iface.getColor(stage, act));
 		g2.draw(s);
 		g2.fill(s);
 	}
+
+
 }
