512952f96d64bcc2f5693f5a6bf9f17977c56352
bemkuive <bemkuive@1b5adb80-8662-4049-b0b6-9cde8559a458>
2011-03-24 14:33:36 +0000

    Wrote a bitmap for collision detection
    git-svn-id: http://svn.aim.avans.nl/svn/1011_TI13_A1_FestivalPlanner/branches/Bart@244 1b5adb80-8662-4049-b0b6-9cde8559a458

---
+++ simulator/Block.java
@@ -0,0 +1,37 @@
+
+public class Block {
+	private final static int height = 6;
+	private final static int width = 6;
+	private boolean point[][] = new boolean[width][height];
+	private boolean allowed = true;
+	public Block()
+	{
+		for (int i = 0; i < width*height; i++)
+		{
+			point[i/width][i%height] = false;
+		}
+	}
+	
+	public boolean claim(int x, int y)
+	{
+		if (allowed == false)
+		{
+			return false;
+		}
+		if (point[x][y] == false)
+		{
+			point [x][y] = true;
+			return true;
+		}
+		return false;
+	}
+	public boolean free(int x, int y)
+	{
+		if (point [x][y] == true)
+		{
+			point [x][y] = false;
+			return true;
+		}
+		return false;
+	}
+}
